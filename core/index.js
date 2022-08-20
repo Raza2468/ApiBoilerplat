65c298cea04184cbbdfa360e49b790c3c6c6c589
MuhammadHammadAhmed <42751169+MuhammadHammadAhmed@users.noreply.github.com>
2022-08-20 15:11:50 +0500

    dbURICommit

--- core/index.js
+++ core/index.js
@@ -1,7 +1,11 @@
 const PORT = process.env.PORT || 5000;
-const dbURI = process.env.dbURI || "mongodb+srv://quiz:2468@quiz.zkk8cdf.mongodb.net/?retryWrites=true&w=majority";
+const dbURI =
+  process.env.dbURI ||
+  "mongodb+srv://quiz:2468@quiz.zkk8cdf.mongodb.net/?retryWrites=true&w=majority";
+
+//change dburl put create cluster url
 
 module.exports = {
-    PORT: PORT,
-    dbURI: dbURI,
-}
+  PORT: PORT,
+  dbURI: dbURI,
+};
